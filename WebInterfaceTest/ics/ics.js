c0ac45a48be4f0c12c2a64557af1af5d2e94c009
Bhaskar Kandiyal <bkandiyal@gmail.com>
2012-05-31 01:47:01 +0530

    Fixed bug when menu not hiding on click

--- WebInterfaceTest/ics/ics.js
+++ WebInterfaceTest/ics/ics.js
@@ -1,5 +1,8 @@
 $(function() {
-	$('#actionButton').click(function() {
+	$('#actionButton').click(function(e) {
+		
+		hideMenu();
+		
 		$('#menu ul').show();
 		$('#menu ul').position({
 			my: "right top",
@@ -8,12 +11,26 @@ $(function() {
 			collision: "fit"
 		});
 		$('#menu ul').hide();
-		$('#menu ul').slideDown();	
+		$('#menu ul').slideDown();
+		$('#menu ul').addClass('shown');
 	});
 	
 	$('#actionButton').blur(function() { 
-		$('#menu ul').hide('slideUp', function() {
-			$('#menu ul').css('top', '600%'); // Hide the element from view
-		});
+		hideMenu();
 	});
 });
+
+function hideMenu()
+{
+	var classes = $('#menu ul').attr('class');
+	if(!classes || classes.indexOf('shown') < 0)
+	{
+		return;
+	}
+		
+	$('#menu ul').hide('slideUp', function() 
+	{
+			$('#menu ul').css('top', '600%'); // Hide the element from view
+			$('#menu ul').removeClass('shown');
+	});
+}
