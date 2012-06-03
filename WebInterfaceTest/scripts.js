fde96eb89de4b58f3e71f7a0e0c6b4387afcf89e
Bhaskar Kandiyal <bkandiyal@gmail.com>
2012-06-04 02:36:56 +0530

    Lots of subtle UI changes and notification support

--- WebInterfaceTest/scripts.js
+++ WebInterfaceTest/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 	
 	$('.nav-active').show();
 	$('#nav-home').parent().addClass('nav-active'); // Set home as the default active link
+	$('#content-home').addClass('content-active');
 	
 	$('div').filter(function() {
 		return this.id.match(/content-[^home].*/);
@@ -23,11 +24,7 @@ $(document).ready(function() {
 function initialize() {
 	$('<div />').addClass('lightbox_bg').appendTo('body').show();
 	$('<div />').html('<p>Please wait....</p><img src=\'images/ajax-loader.gif\'/>')
-	.addClass('modal').appendTo('body');
-	
-	//TODO: Remove the loading screen after the actual loading is finished, 
-	//for now just display it for a set interval
-	//setTimeout(function() { $('.lightbox_bg').remove(); $('.modal').remove(); }, 5 * 1000);
+	.addClass('modal').appendTo('body');	
 }
 
 function setupUI() {
@@ -40,14 +37,6 @@ function setupUI() {
 	$('div').filter(function() {
 		return this.id.match(/^content-[^home].*/ig);
 	}).prepend(navcontent);
-	
-	/*$('button').button();
-	$('input[type=button]').button();
-	$('.button-add').button({icons:{primary:'ui-icon-circle-plus'}});
-	$('.button-delete').button({icons:{primary:'ui-icon-circle-close'}});
-	$('.button-edit').button({icons:{primary:'ui-icon-pencil'}});
-	$('.button-save').button({icons:{primary:'ui-icon-check'}}).hide();
-	$('.button-cancel').button({icons:{primary:'ui-icon-close'}}).hide();*/
 }
 
 function setupEvents() {
@@ -80,32 +69,87 @@ function setupEvents() {
 	});
 	
 	// Show textarea when user clicks the edit button on a note
-	$('.button-edit').live('click', function() {
+	$('.button-edit').live('click', function(e) {
+		e.preventDefault();
 		var parent = $(this).parent();
 		var id = parent.attr('id');
+		console.log(id);
 		id = id.split('-')[2];
-		var content = parent.children('p');
-		var text = content.text();
-		content.replaceWith('<textarea id=\'note-text-'+id+'\'>'+text+'</textarea>');
-		content = parent.children('textarea');
-		// -- Just for animation --
-		content.hide();
-		content.show('slideDown');
-		// -- --
-		$(this).hide();
-		parent.children('.button-edit').hide();
-		parent.children('.button-save').show();
-		parent.children('.button-cancel').show();
-		parent.children('.button-save').click(function() {
-			//TODO: Add code to save note
-		});
-		parent.children('.button-cancel').click(function() {
-			parent.children('.button-save').hide();
-			parent.children('.button-cancel').hide();
-			parent.children('.button-edit').show();
-			parent.children('textarea').replaceWith('<p>'+text+'</p>');
+		
+		$('#note-content-'+id).hide();
+		$('#note-edit-'+id).show('slideDown');
+		
+		console.log('#note-edit-'+id);
+			
+		$('#note-edit-'+id+' .button-save').click(function() {
+			notify('Saving note....', 'alert-info');
+			updateNote(id);
 		});
 		
+		$('#note-edit-'+id+' .button-cancel').click(function() {
+			$('#note-edit-'+id).hide();
+			$('#note-content-'+id).show();
+		});
+		
+	});
+	
+	$('.button-delete').live('click', function() {
+		var parent = $(this).parent();
+		var id = parent.attr('id');
+		console.log(id);
+		id = id.split('-')[2];
+		
+		dialog = '<div id="delete-confirm-modal-'+id+'" class="modal hide">'+
+				 '<div class="modal-body">'+
+				 '<p>Are you sure you wish to delete this item?</p>'+
+				 '</div>'+
+				 '<div class="modal-footer">'+
+				 '<a id="btn-close" href="#" class="btn" data-dismiss="modal">Cancel</a>'+
+				 '<a id="btn-delete" href="#" class="btn btn-primary">Delete</a>'+
+				 '</div></div>';
+		
+		$('body').append(dialog);
+		
+		$('#delete-confirm-modal-'+id+' #btn-close').click(function(e) {
+			e.preventDefault();
+			$('#delete-confirm-modal-'+id).modal('hide');
+			$('#delete-confirm-modal-'+id).remove();
+		});
+		
+		$('#delete-confirm-modal-'+id+' #btn-delete').click(function() {
+			deleteNote(id);
+			$('#delete-confirm-modal-'+id).modal('hide');
+			$('#delete-confirm-modal-'+id).remove();
+		});
+		
+		$('#delete-confirm-modal-'+id).modal();
+	});
+	
+	$('.button-note-add').live('click', function() {
+		$('#add-note-modal').modal();
+	});
+	
+	// Toggle note selection
+	$('#notepad-action-toggle').live('click', function() {
+		$('.note-select').toggle();
+		// Deselect all notes
+		$('#content-notepad .table-list input:checkbox').attr('checked', false);
+	});
+	
+	// Select all notes
+	$('#notepad-action-selectall').live('click', function() {
+		$('#content-notepad .table-list input:checkbox').attr('checked', true);
+	});
+	
+	// Deselect all notes
+	$('#notepad-action-deselectall').live('click', function() {
+		$('#content-notepad .table-list input:checkbox').attr('checked', false);
+	});
+	
+	$('#btn-note-add').live('click', function() {
+		notify('Saving note....', 'alert-info');
+		addNewNote('#form-note-add');
+		$('#add-note-modal').modal('hide');
 	});
 }
 
@@ -123,6 +167,8 @@ function switchTo(id) {
 	
 	$(fromcontid).hide('slideUp', function() { $(contentid).slideDown({duration:'slow'}); });
 	
+	$(fromcontid).removeClass('content-active');
+	$(contentid).addClass('content-active');
 	$(contentid).focus();
 	
 	if(id == 'notepad') { refreshNotes(); }
@@ -133,14 +179,62 @@ function removeActiveClass() {
 }
 
 
+function insertNote(id, title, text, createdDate, modifiedDate)
+{
+	note = $('#content-notepad .table-list tbody');
+	note.append('<tr><td class="note-select hide">'+
+				'<input type="checkbox"/></td>'+
+				'<td><a href="#" id="note-'+id+'">'+title+'</a>'+
+				'<div class="table-hide" id="note-hide-'+id+'">'+
+				'<span id="note-content-'+id+'">'+
+				'<pre>'+text+'</pre><hr/>'+
+				'<button class="button-note-edit button-edit btn">Edit</button>'+
+				'<button class="button-note-delete button-delete btn">Delete</button>'+
+				'</span></div>');
+	
+	table_hide_edit = '<span id="note-edit-'+id+'" class="hide"><form>'+
+					  '<input name="title" type="text" value="'+title+'"/>'+
+					  '<textarea name="text">'+text+'</textarea>'+
+					  '<input type="hidden" name="id" value="'+id+'"/></form>'+
+					  '<button class="button-note-save button-save btn">Save</button>'+
+					  '<button class="button-note-cancel button-cancel btn">Cancel</button>'+
+					  '</span>';
+					  
+	note.append('</tr></td>');
+	
+	$('.table-hide #note-content-'+id).parent().append(table_hide_edit);
+}
+
+function notify(text, type, container, persist)
+{
+	container = (typeof container === "undefined")?'#notification-wrapper':container;
+	persist = (typeof persist === "undefined")?false:persist;
+	
+	id = Math.round(Math.random()*100);
+	content = '<div id="'+id+'" class="'+type+' alert hide notification">'+
+	'<a href="#" class="close" data-dismiss="alert">x</a>'+
+	text+'</div>';
+	
+	//console.log(container);
+	
+	$(container).prepend(content);
+	$('#'+id).fadeIn();
+	
+	// Hide notification after 5 seconds
+	setTimeout("$('#"+id+"').fadeOut('slow', function() { $(this).remove() })", 5 * 1000); 
+}
+
+/* AJAX Methods */
+
+URL = "http://localhost/server/index.php";
+
 function refreshNotes()
 {
-	URL = "http://localhost/server/index.php";
 	
 	// Get all notes
 	$.getJSON(URL+'/notes/get', function(data) {
 		if(data.code != 200) {
-			alert("Error fetching data. The server says\n"+data['msg']);
+			notify("Error fetching data. The server says\n"+data['msg'], 'alert-error');
 		}
 		else {
 			$('#content-notepad .table-list').remove();
@@ -155,13 +249,52 @@ function refreshNotes()
 	});
 }
 
-function insertNote(id, title, text, createdDate, modifiedDate)
+function addNewNote(form)
 {
-	note = $('#content-notepad .table-list tbody');
-	note.append('<tr><td><a href="#" id="note-'+id+'">'+title+
-											'</a><div id="note-content-'+id+'" class="table-hide"><p>'+text+'</p><hr/><button class="button-note-edit'+
-											' button-edit btn">Edit</button>'+
-											'<button class="button-note-save button-save btn hide">Save</button>'+
-											'<button class="button-note-cancel button-cancel btn hide">Cancel</button>');
-	note.append('</div></tr></td>');
+	$.post(URL+'/notes/new', $(form).serialize(), function(data) {
+		if(data.code != 200) {
+			//alert("Error adding new note. The server says\n"+data['msg']); 
+			notify('An error occurred while adding a new note<br/>The server says: '+data['msg']);
+		}
+		else {
+			refreshNotes();
+			if($('#add-note-modal').is(':hidden')) {
+				notify('Note saved successfully!', 'alert-success');
+			}
+			else {
+				notify('Note saved successfully!', 'alert-success', '#add-note-modal .modal-body');
+			}
+		}
+	}, 'json');
+}
+
+function updateNote(id)
+{
+	postData = $('#note-edit-'+id+' form').serialize();
+	
+	$.post(URL+'/notes/update', postData, function(data) {
+		//console.log(data.code);
+		if(data.code != 200) {
+			notify('Error updating note: '+data['msg'], 'alert-error');
+		}
+		else {
+			refreshNotes();
+			notify('Note updated successfully!', 'alert-success');
+		}
+	}, 'json');
+}
+
+function deleteNote(id)
+{
+	$.getJSON(URL+'/notes/delete/'+id, function(data) {
+		if(data.code != 200)
+		{
+			notify('Error deleting note: '+data['msg'], 'alert-error');
+		}
+		else
+		{
+			refreshNotes();
+			notify('Note deleted successfully!', 'alert-success');
+		}
+	});	
 }
