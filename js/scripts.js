164d00b3f23d4d502861bba1e998aba63dae909c
Bhaskar Kandiyal <bkandiyal@gmail.com>
2012-07-21 16:38:05 +0530

    Improved the home page

--- js/scripts.js
+++ js/scripts.js
@@ -144,7 +144,7 @@ function setupEvents() {
 	});
 	
 	// Show settings dialog when settings menu item is clicked
-	$('#showSettings').click(function() {
+	$('.showSettings').click(function() {
 		$('#settings').addClass('modal');
 		$('#settings').modal();
 	});
@@ -238,9 +238,9 @@ function switchTo(id) {
 }
 
 function addApplication(app) {
-	li = '<li><a href="#" id="nav-'+app['name']+'" data-switch="'+app['name']+'"><img src="'+app['icon-small']+'"/>'+app['title']+'</a></li>';
+	li = '<li><a href="#" id="nav-'+app['name']+'" data-switch="'+app['name']+'" title="'+app['title']+'"><img src="'+app['icon-small']+'"/>'+app['title']+'</a></li>';
 	$('#nav .nav-list').append(li);
-	tr = '<tr><td><a href="#" data-switch="'+app['name']+'"><img src="'+app['icon-big']+'"/>'+app['title']+'</a></td></tr>';
+	tr = '<tr><td><a href="#" data-switch="'+app['name']+'" title="'+app['title']+'"><img src="'+app['icon-big']+'"/>'+app['title']+'</a></td></tr>';
 	$('#table-launchers').append(tr);
 	refreshUI();
 }
